54911e9c72c698660c754fcf0ae4c44326ab894e
Frozen owner <156554454+prashantsahlot@users.noreply.github.com>
2024-08-23 14:56:35 +0530

    Update musicdownload.py

--- musicdownload.py
+++ musicdownload.py
@@ -9,7 +9,7 @@ import time
 import random
 
 # Telegram bot token
-TOKEN = '7186451521:AAFudMLZOFAshnCMpQbloV7iKodztcpImsY'
+TOKEN = '7277031813:AAHRtoG1bGcOuZeZB3ENoXhMAYybR87611Y'
 
 # Initialize bot
 bot = telebot.TeleBot(TOKEN)
